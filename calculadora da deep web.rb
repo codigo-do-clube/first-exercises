2326569ea96de40f62c7913c051eab3919540009
rodrigo gomes costa <rodrigogomesc1997@gmail.com>
2020-11-16 18:23:01 -0300

    calculadora teste

---
+++ calculadora da deep web.rb
@@ -0,0 +1,26 @@
+=begin
+teste calculadora que provavelmente vai dar errado e aproveitando
+para
+testar
+comentarios
+multi
+linha
+=end
+
+class 
+    def calculadora.somar(a,b)
+    a+b
+end
+def Calculadora.subtração(a,b)
+    a-b
+end
+def Caluladora.divisão(a,b)
+    a/b
+end
+def Calculadora.multiplicação(a,b)
+    a*b
+end
+end
+
+Print "Insira o primeiro valor:"
+valor1 = gets.chomp
