55db7eae17045d341ecdd6bc2f1e1e6158da2fcc
rodrigo gomes costa <rodrigogomesc1997@gmail.com>
2020-11-16 19:10:25 -0300

    test

--- calculadora da deep web.rb
+++ calculadora da deep web.rb
@@ -22,5 +22,23 @@ def Calculadora.multiplicação(a,b)
 end
 end
 
-Print "Insira o primeiro valor:"
+print "insira o primeiro valor"
 valor1 = gets.chomp
+
+print "insira o segundo valor"
+valor2 = gets.chomp
+
+print "selecione a operação (soma, subtração, divisão, multiplicação"
+
+puts case selecione a operação
+when "+" : Calculadora.somar(valor1.to_i,valor2.to_i)
+when "-" : Calculadora.subtração(valor1.to_i,valor2.to_i)
+when "/" : Calculadora.divisão(valor1.to_i,valor2.to_i)
+when "*" : Calculadora.multiplicação(valor1.to_i,valor2.to_i)
+
+end
+
+valor final = valor1.to_i.send(operador.to_sym,last.to_i)
+print. "o valor final é: #{valor final}"
+
+end
